1fad4d9dbe0540eada2082fce8e6b82bf5db2f8e
Lynn <lynn.chebib@net.usj.edu.lb>
2026-04-16 23:44:53 +0300

    sample_user_change

--- tests/test_manual_auth.py
+++ tests/test_manual_auth.py
@@ -2,8 +2,8 @@ import requests
 from app.config import settings
 
 WEB_API_KEY = settings.firebase_web_api_key
-TEST_EMAIL    = "test@test.com"
-TEST_PASSWORD = "Test1234@"
+TEST_EMAIL    = "christy.n.chamoun@gmail.com"
+TEST_PASSWORD = "TestTest123@!"
 
 response = requests.post(
     f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={WEB_API_KEY}",
